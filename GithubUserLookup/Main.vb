3c6e5bf8a439d7d82249df069157f3a04378b007
chrisfrear <chris.frear@hotmail.co.uk>
2025-07-01 22:27:44 +0100

    Add textbox autocomplete

--- GithubUserLookup/Main.vb
+++ GithubUserLookup/Main.vb
@@ -1,8 +1,11 @@
-﻿Imports System.IO
+﻿Imports System.Buffers.Text
+Imports System.IO
+Imports System.Net
 Imports System.Net.Cache
 Imports System.Net.Http
 
 Public Class frm_Main
+    Dim SearchHistoryPath As String = IO.Path.GetFullPath(Application.StartupPath & "\..\..\..\Resources\") & "searchhistory.txt"
 
     Public Sub btn_Search_Click() Handles btn_Search.Click
 
@@ -40,48 +43,46 @@ Public Class frm_Main
         Dim apiUrl As String = "https://api.github.com/users/"
         Dim user As New User
 
-        Dim client As New HttpClient()
-        client.BaseAddress = New Uri(apiUrl)
-        client.DefaultRequestHeaders.Accept.Clear()
-        client.DefaultRequestHeaders.Accept.Add(New System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github+json"))
-        client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "http://developer.github.com/v3/#user-agent-required")
+        Using client As New HttpClient()
+            client.BaseAddress = New Uri(apiUrl)
+            client.DefaultRequestHeaders.Accept.Clear()
+            client.DefaultRequestHeaders.Accept.Add(New System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github+json"))
+            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "http://developer.github.com/v3/#user-agent-required")
 
-        Dim response As HttpResponseMessage
-        response = client.GetAsync(tb_Username.Text).Result
+            Dim response As HttpResponseMessage
+            response = client.GetAsync(tb_Username.Text).Result
 
-        If response.IsSuccessStatusCode Then
-            Dim data As String = Await response.Content.ReadAsStringAsync()
-            user = Newtonsoft.Json.JsonConvert.DeserializeObject(Of User)(data)
-            Return user
-        Else
-            Return Nothing
-        End If
-
-        client.Dispose()
+            If response.IsSuccessStatusCode Then
+                Dim data As String = Await response.Content.ReadAsStringAsync()
+                user = Newtonsoft.Json.JsonConvert.DeserializeObject(Of User)(data)
+                Return user
+            Else
+                Return Nothing
+            End If
+        End Using
     End Function
 
     Public Async Function GetRepos(repoUrl As String) As Task(Of List(Of Repo))
 
-        Dim client As New HttpClient()
-        client.BaseAddress = New Uri(repoUrl)
-        client.DefaultRequestHeaders.Accept.Clear()
-        client.DefaultRequestHeaders.Accept.Add(New System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github+json"))
-        client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "http://developer.github.com/v3/#user-agent-required")
+        Using client As New HttpClient()
+            client.BaseAddress = New Uri(repoUrl & "?per_page=100")
+            client.DefaultRequestHeaders.Accept.Clear()
+            client.DefaultRequestHeaders.Accept.Add(New System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github+json"))
+            client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "http://developer.github.com/v3/#user-agent-required")
 
-        Dim response As HttpResponseMessage
-        response = client.GetAsync("").Result
+            Dim response As HttpResponseMessage
+            response = client.GetAsync("").Result
 
-        If response.IsSuccessStatusCode Then
-            Dim data As String = Await response.Content.ReadAsStringAsync()
-            Dim repos As New List(Of Repo)
+            If response.IsSuccessStatusCode Then
+                Dim data As String = Await response.Content.ReadAsStringAsync()
+                Dim repos As New List(Of Repo)
 
-            repos = Newtonsoft.Json.JsonConvert.DeserializeObject(Of List(Of Repo))(data)
-            Return repos
-        Else
-            Return Nothing
-        End If
-
-        client.Dispose()
+                repos = Newtonsoft.Json.JsonConvert.DeserializeObject(Of List(Of Repo))(data)
+                Return repos
+            Else
+                Return Nothing
+            End If
+        End Using
     End Function
 
     Private Sub tb_Username_KeyUp(sender As Object, e As KeyEventArgs) Handles tb_Username.KeyUp
@@ -91,23 +92,33 @@ Public Class frm_Main
     End Sub
 
     Private Sub AddToSearchHistory()
-        Dim path As String = IO.Path.GetFullPath(Application.StartupPath & "\..\..\..\Resources\") & "searchhistory.txt"
-        If File.Exists(path) = False Then
+
+        If File.Exists(SearchHistoryPath) = False Then
             'Create a file to write to.
-            Using sw As StreamWriter = File.CreateText(path)
+            Using sw As StreamWriter = File.CreateText(SearchHistoryPath)
                 sw.WriteLine(tb_Username.Text)
             End Using
         Else
             'Write to existing file
-            File.AppendAllText(path, tb_Username.Text & Environment.NewLine)
+            File.AppendAllText(SearchHistoryPath, tb_Username.Text & Environment.NewLine)
         End If
+    End Sub
 
-        Using sr As StreamReader = New StreamReader(path)
-            sr.
-        End Using
+    Private Sub ReadSearchHistory()
         Dim history As New AutoCompleteStringCollection
-        history =
-        tb_Username.AutoCompleteCustomSource =
 
+        Using sr As StreamReader = New StreamReader(SearchHistoryPath)
+            Dim line As String = sr.ReadLine()
+            Do While Not line = Nothing
+                history.Add(line)
+                line = sr.ReadLine()
+            Loop
+        End Using
+
+        tb_Username.AutoCompleteCustomSource = history
+    End Sub
+
+    Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReadSearchHistory()
     End Sub
 End Class
